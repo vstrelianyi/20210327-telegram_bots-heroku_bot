09afef7d17c04b3b6689e1c1cf8b47b2eb71f037
viktor_strelianyi <chmpain@gmail.com>
2021-03-27 14:13:41 +0200

    - udpate

---
+++ index.js
@@ -0,0 +1,16 @@
+require( 'dotenv' ).config(); // writes all key-value pairs from .env to process.env
+
+const Telegraf = require( 'telegraf' );
+
+const { BOT_TOKEN, } = process.env;
+
+const bot = new Telegraf( BOT_TOKEN );
+
+bot.start( ctx => ctx.reply( 'Welcome' ) );
+bot.help( ctx => ctx.reply( 'Send me a sticker' ) );
+bot.on( 'sticker', ctx => ctx.reply( 'Great' ) );
+bot.hears( 'hi', ctx => ctx.reply( 'hey there' ) );
+
+bot.launch()
+  .then( res => console.log( `Launched at ${ new Date() }` ) )
+  .catch( err => console.log( 'Error at launch', err ) );
